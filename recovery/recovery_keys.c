29d0b34662bebb186cae21794bc4146405441b04
Nelson <nsardinha3@gmail.com>
2012-03-19 21:14:25 -0400

    touch recovery keymaping

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -14,33 +14,54 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 0;
         //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_BACK);
+    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
 
 int device_handle_key(int key_code, int visible) {
+    printf("handling a key! %i\n", key_code);
     if (visible) {
         switch (key_code) {
             case KEY_CAPSLOCK:
             case KEY_DOWN:
             case KEY_VOLUMEDOWN:
+            case KEY_MENU:
                 return HIGHLIGHT_DOWN;
 
             case KEY_LEFTSHIFT:
             case KEY_UP:
             case KEY_VOLUMEUP:
-                return HIGHLIGHT_UP;
-
-            case KEY_HOME:
-                return SELECT_ITEM;
+            case KEY_BACK:
+               return HIGHLIGHT_UP;   
 
             case KEY_POWER:
-                if (ui_get_showing_back_button())
+                if (ui_get_showing_back_button()) {
                     return SELECT_ITEM;
-                if (!get_allow_toggle_display())
+                }
+                if (!get_allow_toggle_display() && ui_menu_level > 0) {
                     return GO_BACK;
+                }
                 break;
-            default:
-                return NO_ACTION;
+            case KEY_LEFTBRACE:
+            case KEY_ENTER:
+            case BTN_MOUSE:
+            case KEY_CAMERA:
+            case KEY_F21:
+            case KEY_SEND:
+                return SELECT_ITEM;
+            
+            case KEY_END:
+            case KEY_BACKSPACE:
+            case KEY_SEARCH:
+                if (ui_get_showing_back_button()) {
+                    return SELECT_ITEM;
+                }
+                if (!get_allow_toggle_display() && ui_menu_level > 0) {
+                    return GO_BACK;
+                }
+            case KEY_HOME:
+                if (ui_menu_level > 0) {
+                    return GO_BACK;
+                }
         }
     }
 
